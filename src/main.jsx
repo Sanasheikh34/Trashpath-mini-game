64700610438e56730f18d7ac6d7a793134602fc1
Sanasheikh34 <121682534+Sanasheikh34@users.noreply.github.com>
2025-07-01 01:38:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TrashPath from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TrashPath from './App.jsx';
+import './index.css';
+
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const GORBAGANA_RPC = 'https://rpc.gorbagana.wtf';
+
+const wallets = [new BackpackWalletAdapter()];
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TrashPath />
-  </React.StrictMode>,
-)
+    <ConnectionProvider endpoint={GORBAGANA_RPC}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <TrashPath />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  </React.StrictMode>
+);
