1387d8a0786eddafda051bbe66af69a6ff4ddbd1
Sanasheikh34 <121682534+Sanasheikh34@users.noreply.github.com>
2025-07-01 00:38:23 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TrashPath from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TrashPath />
+  </React.StrictMode>,
+)
